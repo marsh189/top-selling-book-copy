473d8eeb2f2d72471c64b665040bf5d0ee3a3010
Matthew Marshall <marsh189@mail.chapman.edu>
2024-05-21 15:13:29 -0700

    First Commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import onyx from './images/onyx.jpg'
+import women from './images/women.jpg'
+import dogman from './images/dogman.jpg'
+
+export const books = [
+  {
+    author: 'Rebecca Yarros',
+    title: 'Onyx Storm',
+    img: onyx,
+    id: 1,
+  },
+  {
+    author: 'Kristin Hannah',
+    title: 'The Women: A Novel',
+    img: women,
+    id: 2,
+  },
+  {
+    author: 'Dav Pilkey',
+    title: 'Dog Man: The Scarlet Sheddar',
+    img: dogman,
+    id: 3,
+  },
+]
